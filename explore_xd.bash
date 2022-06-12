77d4ffbd417b990ed24b391005340588517a1c21
goodnightkiss1972 <goodnightkiss1972@gmail.com>
2022-06-12 23:05:00 +0200

    on dirait qu'on a une extraction

--- explore_xd.bash
+++ explore_xd.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-file_xd=all_original_library_file
-for i in {0..5}
+file_xd=originals_and_64_bobeats
+for i in {199..264}
 do
-  echo "python3 mnlgxd.py $file_xd.mnlgxdlib $i > $file_xd/$i.txt"
+  python3 mnlgxd.py $file_xd.mnlgxdlib $i > $file_xd/$i.txt
 done
